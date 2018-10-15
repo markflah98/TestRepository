2494544d2f5dd6e7f2d120fd7d1484be4df6dfe4
MARK FLAHERTY - STUDENT <G00356549@gmit.ie>
2018-10-15 09:28:19 +0100

    Add project files.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+ private void ToolbarItem_Clicked(object sender, EventArgs e)
+{
+
+    ToolbarItem current = (ToolbarItem)sender;
+    if( current.Text == "Add")
+    {
+        Navigation,PushAsync(new NewItemPage)
+    }
+}
